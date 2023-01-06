5d651ef503ab490397a8faab41c010228f9876f3
ajitkk87 <invincibleajitkk@gmail.com>
2023-01-06 14:12:48 +0530

    User rest service added

---
+++ restws/src/main/java/com/ajit/restws/user/UserController.java
@@ -0,0 +1,57 @@
+package com.ajit.restws.user;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+public class UserController {
+	private UserDaoService service;
+
+	public UserController(UserDaoService service) {
+		this.service = service;
+	}
+
+	//http://localhost:8080/users
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers() {
+		return service.findAll();
+	}
+	
+	@GetMapping("/users/{id}")
+	public User retrieveUser(@PathVariable int id) throws Exception {
+		User user = service.findOne(id);
+		
+		if(user==null)
+			throw new Exception("User not found for id:"+id);
+		
+		return user;
+	}
+	
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		service.deleteById(id);
+	}
+
+	@PostMapping("/users")
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		
+		User savedUser = service.save(user);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+						.path("/{id}")
+						.buildAndExpand(savedUser.getId())
+						.toUri();   
+		
+		return ResponseEntity.created(location).build();
+	}
+
+}
